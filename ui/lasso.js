eb55829a3867249795be5eb7f6126517ed6cfed2
Charles Read <physixrox@gmail.com>
2017-02-12 15:27:23 -0500

    ui

--- ui/lasso.js
+++ ui/lasso.js
@@ -4,6 +4,8 @@ const path = require('path')
 const baseDir = path.resolve(path.join(__dirname, '..', '..', '..'))
 const lasso = require('lasso')
 
+console.log(baseDir)
+
 lasso.configure({
   outputDir: path.join(baseDir, 'static'),
   plugins: [
