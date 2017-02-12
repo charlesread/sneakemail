c45dbfd691aed32e14cd4870e06bf8d966d37fd3
Charles Read <physixrox@gmail.com>
2017-02-12 15:35:51 -0500

    ui

--- ui/lasso.js
+++ ui/lasso.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+// this puts the baseUrl at the root of the actual project, not this module
 const baseDir = path.resolve(path.join(__dirname, '..', '..', '..'))
 const lasso = require('lasso')
 
